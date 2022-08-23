3e8cdcf2dadf9de51f8da301edba3d7591a474d8
onium16 <onium16@gmail.com>
2022-08-23 19:26:32 +0300

    add

--- testflask.py
+++ testflask.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return "Привет!"
+
+@app.route('/test')
+def test():
+    return "<h1>TEST_PAGE</h1>"
  
  
 if __name__ == '__main__':
